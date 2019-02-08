d06d014c125f73f9a375ff3e7f10108a5551e076
diogoalmeida1709 <diogoalmeida1709@gmail.com>
2019-02-08 17:22:46 +0100

    Start of the project and navbar datas

---
+++ html/templates/navbar.php
@@ -0,0 +1,17 @@
+<nav class="light-blue lighten-1" role="navigation">
+  <div class="nav-wrapper container">
+    <!-- Computer's version -->
+    <ul class="right hide-on-med-and-down">
+      <li><a href="#">Results</a></li>
+      <li><a href="#">Ranking</a></li>
+      <li><a href="#">Login / Register</a></li>
+    </ul>
+    <!-- Mobile's version -->
+    <ul id="nav-mobile" class="sidenav">
+      <li><a href="#">Results</a></li>
+      <li><a href="#">Ranking</a></li>
+      <li><a href="#">Login / Register</a></li>
+    </ul>
+    <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+  </div>
+</nav>
